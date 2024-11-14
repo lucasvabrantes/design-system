70181ea8183e3d4e141cac2ff9a2d055afb6084a
lucas <lucasabrts@icloud.com>
2024-11-14 14:20:33 -0300

    feat: create sourev input

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
-import { Input } from "@/components/ui/input";
+import { SrInput } from "@/components/shared/input";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-items-center items-center min-h-screen p-12 gap-3">
-      <Input label="Label" placeholder="Design System" />
-      <Input label="Label" hint="Hint" placeholder="Design System With Hint" />
-      <Input disabled placeholder="Design System Disabled" />
-      <Input icon="ri-tent-fill" placeholder="Design System With Icons" />
+      <SrInput label="Label" placeholder="Design System" />
+      <SrInput
+        label="Label"
+        hint="Hint"
+        placeholder="Design System With Hint"
+      />
+      <SrInput disabled placeholder="Design System Disabled" />
+      <SrInput icon="ri-tent-fill" placeholder="Design System With Icons" />
     </div>
   );
 }
--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,45 +1,19 @@
 import * as React from "react";
+
 import { cn } from "@/lib/utils";
-import "remixicon/fonts/remixicon.css";
-import { Label } from "./label";
 
-interface InputProps extends React.ComponentProps<"input"> {
-  label?: string;
-  hint?: string;
-  icon?: string;
-}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, label, hint, icon, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
+  ({ className, type, ...props }, ref) => {
     return (
-      <div>
-        {label && (
-          <Label htmlFor={props.id} className="text-sm">
-            {label}
-          </Label>
+      <input
+        type={type}
+        className={cn(
+          "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
         )}
-        <div className="flex justify-between relative">
-          <i
-            className={`${icon} absolute left-2 top-[0.875rem] text-gray-400 h-2 w-2`}
-          ></i>
-          <input
-            type={type}
-            className={cn(
-              "flex h-13 w-80 border-2 rounded bg-background px-5 py-2 text-base transition-colors placeholder:text-gray-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 disabled:border-gray-500 md:text-sm",
-              hint ? "border-red-500" : "",
-              hint ? "focus:border-red-500" : "",
-              className
-            )}
-            ref={ref}
-            {...props}
-          />
-
-          <i
-            className={`${icon} absolute right-2 top-[0.875rem] text-gray-400 h-2 w-2`}
-          ></i>
-        </div>
-        {hint && <p className="text-xs mt-[2px]">{hint}</p>}
-      </div>
+        ref={ref}
+        {...props}
+      />
     );
   }
 );
